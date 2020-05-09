703e91b386a521c191dc1a08bef47089eeae1f68
PUNKLANCER <punklancer99@gmail.com>
2020-05-09 11:01:45 +0530

    Updates

---
+++ precache-manifest.595465f19f8f71dccba74b3f86a1b53e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd5c2bf36a21fd3459173083bedb427",
+    "url": "/index.html"
+  },
+  {
+    "revision": "155520c129548449f706",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "7d1a698619de9dd50c4b",
+    "url": "/static/css/main.0a2f9faa.chunk.css"
+  },
+  {
+    "revision": "155520c129548449f706",
+    "url": "/static/js/2.d82cd422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d82cd422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d1a698619de9dd50c4b",
+    "url": "/static/js/main.9f3424de.chunk.js"
+  },
+  {
+    "revision": "df16bcf85c25d69dad33",
+    "url": "/static/js/runtime-main.004a6136.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
